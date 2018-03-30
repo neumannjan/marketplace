ed58894a14d9b9b8911e7fdaec232144d8604cd2
Jan Neumann <janneumannprg@gmail.com>
2018-03-30 20:19:10 +0200

    Added route guard to admin routes

--- resources/assets/js/components/routes/admin/index.vue
+++ resources/assets/js/components/routes/admin/index.vue
@@ -7,11 +7,13 @@
 <script lang="ts">
     import { Vue, Component, mixins } from "JS/components/class-component";
     import route from "JS/components/mixins/route";
+    import routeGuard from "JS/components/mixins/route-guard";
+    import store from "JS/store";
 
     @Component({
         name: 'admin-route'
     })
-    export default class AdminRoute extends mixins(route) {
+    export default class AdminRoute extends mixins(route, routeGuard('admin', () => store.state.is_admin)) {
         readonly isTopLevelRoute: boolean = true;
         get title(): string {
             return "Administration";
